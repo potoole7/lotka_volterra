57880eb6abfa3a43c56e6bbb6a2df880e4807a65
paddy7wb <paddy7wb@gmail.com>
2023-09-28 15:22:04 +0100

    Use fe function in main script

--- lotka.py
+++ lotka.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import timestep_functions as ts
 
 # max time
 T = 100
-# n
+# number of time steps
 N = 100000
 # timestep
-dt=T/N
-# time
+# dt=T/N
+# time (for plot)
 t=np.linspace(0,T,N+1)
 # ODE parameters
 a=1.2
@@ -22,12 +23,10 @@ sol = np.zeros((N+1, 2))
 sol[0,0] = 10
 sol[0,1] = 10
 
-# loop using forward euler
-for i in range(N):
-    sol[i+1,0]=sol[i,0]+a*sol[i,0]*dt - b*sol[i,0]*sol[i,1]*dt
-    sol[i+1,1]=sol[i,1]-g*sol[i,1]*dt + d*sol[i,0]*sol[i,1]*dt
+# Forward Euler solution
+sol_fe = ts.f_euler(sol, N, T, a, b, g, d)
 
-plt.plot(t, sol[:,0])
-plt.plot(t, sol[:,1])
+plt.plot(t, sol_fe[:,0])
+plt.plot(t, sol_fe[:,1])
 plt.show()
 
