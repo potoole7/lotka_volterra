9dd7d173edcf38a608325bee47ec299b88b93e4f
Amino21786 <80826453+Amino21786@users.noreply.github.com>
2023-09-29 10:54:33 +0100

    MSE added

--- z_scipy.py
+++ z_scipy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from sklearn.metrics import mean_squared_error
 
 def true_sol(t):
     return np.exp(-1*t)
@@ -11,13 +12,41 @@ def rhs(t, z):
 T = 10
 z0 = np.array([1])
 
-sol = solve_ivp(lambda t,z: rhs(t, z), t_span=[0, T], y0=z0, max_step=0.1)
+steps = [0.2, 0.4, 0.6, 0.8, 1]
 
-t = np.linspace(0, T)
+for step in steps:
+    sol = solve_ivp(lambda t,z: rhs(t, z), t_span=[0, T], y0=z0, max_step=step)
+    t = np.linspace(0, T)
+    mse = mean_squared_error(true_sol(t), sol.y.T)
+    plt.plot(step, mse)
+    plt.legend(step)
+    plt.xlabel('step')
+    plt.ylabel('MSE')
+
+
+#plt.legend('Ground truth')
+plt.savefig('sol_z.png', dpi=300)
+plt.show()
+
+
+
+
+
+
+
+"""
+for step in steps:
+    sol = solve_ivp(lambda t,z: rhs(t, z), t_span=[0, T], y0=z0, max_step=step)
+    t = np.linspace(0, T)
+    plt.plot(sol.t, sol.y.T)
+    label = 'RK-45'
+    plt.legend('RK-45',)
+
+
 sol_true = true_sol(t)
 
-plt.plot(sol.t, sol.y.T)
 plt.plot(t, sol_true)
 plt.xlabel('t')
-plt.legend(['RK45', 'Ground truth'])
+plt.legend('Ground truth')
 plt.savefig('sol_z.png', dpi=300)
+"""
